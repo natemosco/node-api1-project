74b64c045eed9a5411ce4dec89913a70e8a3d4db
Nathaniel Mosco <natemosco@gmail.com>
2019-12-02 23:11:41 -0500

    edited GET to include findById() function

--- index.js
+++ index.js
@@ -16,74 +16,73 @@ server.get("/users", (req, res) => {
     });
 });
 
-server.get("/users/:id",(req,res) => {
-    let id = req.params.id;
-    db.find(id)
-        .then(singleUser => {
-            res.status(200).json(singleUser);
-        })
-        .catch(error =>{
-            console.log("error from /user/:id", error)
-            res.status(404).json({message: "id not found"});
-        });
-});
-
-server.post("/users",(req,res) => {
-    const newUser = req.body;
-    db.insert(newUser)
-    .then((users) => {
-        res.status(201).json(users);
+server.get("/users/:id", (req, res) => {
+  let id = req.params.id;
+  db.findById(id)
+    .then(singleUser => {
+      res.status(200).json(singleUser);
     })
-    .catch((error) => {
-        console.log("error from post /users", error);
-        res.status(500).json({errorMessage: "error adding user to database"})
+    .catch(error => {
+      console.log("error from /user/:id", error);
+      res.status(404).json({ message: "id not found" });
     });
 });
 
-server.delete("/users/:id", (req,res) => {
-    const id = req.params.id;
-
-    db.remove(id)
-    .then((removed) => {
-        if(removed){
-            res.status(200).json({message: "user deleted by id successfully"});
-        }else{
-            res.status(404).json({ message: "user by this id cannot be found" });
-        };
+server.post("/users", (req, res) => {
+  const newUser = req.body;
+  db.insert(newUser)
+    .then(users => {
+      res.status(201).json(users);
     })
-    .catch((errror) => {
-        console.log("error on DELETE /users/:id", error);
-        res.status(500).json({ errorMessage: "error removing this user "})
+    .catch(error => {
+      console.log("error from post /users", error);
+      res.status(500).json({ errorMessage: "error adding user to database" });
     });
 });
 
-server.put("/users/:id", (req,res) => {
-    const id = req.params.id;
-    const update = req.body;
-    
-    db.update(id, update)
-    .then((user) => {
-        res.status(200).json(user);
+server.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(removed => {
+      if (removed) {
+        res.status(200).json({ message: "user deleted by id successfully" });
+      } else {
+        res.status(404).json({ message: "user by this id cannot be found" });
+      }
     })
-    .catch((error) => {
-        console.log("error in put /users/:id", error);
-        res.status(404).json({ errorMessage: "error editing this user"});
+    .catch(errror => {
+      console.log("error on DELETE /users/:id", error);
+      res.status(500).json({ errorMessage: "error removing this user " });
     });
-    
-})
+});
+
+server.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const update = req.body;
+
+  db.update(id, update)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      console.log("error in put /users/:id", error);
+      res.status(404).json({ errorMessage: "error editing this user" });
+    });
+});
 
 const port = 3459;
 server.listen(port, () =>
   console.log(`\n ** API running on port ${port} **\n`)
 );
 
-server.get("/:id", (req,res) => {
-    db.findById()
-    .then(char => {
-        res.status(200).json(char);
-    })
-    .catch(error => {
-        console.log("error from get /:id", error);
-        res.status()
-    })
-}
+// server.get("/:id", (req,res) => {
+//     db.findById()
+//     .then(char => {
+//         res.status(200).json(char);
+//     })
+//     .catch(error => {
+//         console.log("error from get /:id", error);
+//         res.status()
+//     })
+// }
